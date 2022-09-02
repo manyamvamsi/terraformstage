2bbcdbdd98f491c5c226d1927c18aa59b05fabb1
manyamvamsi <manyam.vamsi04@gmail.com>
2022-09-02 15:17:24 +0530

    final-update

---
+++ inputs.tf
@@ -0,0 +1,26 @@
+variable "subscription_id" {
+    type = string
+    default = "ffd24958-0849-41b3-a4ab-d6c8e95d5147"
+    description = "IAC-subscriptionid"
+  
+}
+
+variable "client_id" {
+    type = string
+    default = "9b910e94-1390-4152-a8c4-4f5b93ffaae4"
+    description = "IAC-clientid"
+  
+}
+
+variable "client_secret" {
+    type = string
+    default = "JKl8Q~uwjYTPVxd85EecI~WBPOlkId_dxWLasadj"
+    description = "IAC-clientsecret"
+  
+}
+
+variable "tenant_id" {
+    type = string
+    default = "c911f0e4-2d1e-4fb9-b1cd-37a2caaece23"
+  
+}
---
+++ main.tf
@@ -0,0 +1,61 @@
+terraform {
+  backend "azurerm"{
+    resource_group_name = "datasourcerg"
+    storage_account_name = "datasource112"
+    container_name = "bookstorestate"
+    key = "terraform.bookstorestate"
+    access_key = "EY5HiaqzbgL49mozIJtnOp/w3gZ0bdPEVuy8Ve/wHQwSEMpoi/dyoFYDHZeCGPdJu7I9cTu7/FJ/+AStMigPJw=="
+
+  }
+}
+
+
+provider "azurerm" {
+    features {
+      
+    }
+    subscription_id = var.subscription_id
+    client_id = var.client_id
+    tenant_id = var.tenant_id
+    client_secret = var.client_secret
+}
+locals {
+  setup_name = "practice-hyd"
+}
+
+resource "azurerm_resource_group" "webapprg1" {
+  name = "webapp98"
+  location = "East US"
+  tags = {
+    "name" = "${local.setup_name}-rsg"
+  }  
+}
+
+resource "azurerm_app_service_plan" "appplan11" {
+  name = "appplandev"
+  location = azurerm_resource_group.webapprg1.location
+  resource_group_name = azurerm_resource_group.webapprg1.name
+  sku {
+    tier = "standard"
+    size = "S1"
+  }
+  tags = {
+    "name" = "${local.setup_name}-applan"
+  }
+  depends_on = [
+    azurerm_resource_group.webapprg1
+  ]
+}
+
+resource "azurerm_app_service" "webapp1" {
+  name = "webappdev"
+  location = azurerm_resource_group.webapprg1.location
+  resource_group_name = azurerm_resource_group.webapprg1.name
+  app_service_plan_id = azurerm_app_service_plan.appplan11.id
+  tags = {
+    "name" = "${local.setup_name}-webapp"
+  }
+  depends_on = [
+    azurerm_app_service_plan.appplan11
+  ]
+}
